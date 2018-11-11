6929b0ecf86966b3c0aaa4c47be97dfd680332bb
Mansi_O <mansi.mi11@gmail.com>
2018-11-11 10:07:15 -0800

    test12

--- ui/src/containers/Invoice/index.js
+++ ui/src/containers/Invoice/index.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
-import {Panel, ListGroup, ListGroupItem,Button} from 'react-bootstrap'
+import {Modal,Panel, ListGroup, ListGroupItem,Button, Alert} from 'react-bootstrap'
+
+//call for the push transaction
 
 
-
-
+//***Hardcode for now */
+const contract="abc";
+const transferTokenAccount="tokenName";
+const from="useracctname" ;
+const quantity="20AABB"; 
+const memo="optional" ;
+const auth={actor: 'acctname1234', permission: 'active'};
 
 let inputStyle1 = {
   float:'right',
@@ -20,7 +27,74 @@ let inputStyle2 = {
 };
 
 class Invoice extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleShow = this.handleShow.bind(this);
+    this.handleHide = this.handleHide.bind(this);
+    this.pushTransfer = this.pushTransfer.bind(this);
+
+    //for the pop ups
+    
+    // this.handleShowContractSucesss = this.handleShowContractSucesss.bind(this);
+    // this.handleShowContractFailure = this.handleShowContractFailure.bind(this);
+
+    this.state = {
+      showModel: false,
+      showAlertSucess:false,
+      showAlertFailure:false,
+    };
+  }
+
+   pushTransfer(eos, contract,transferTokenAccount, from, quantity, memo, auth, callback = () => {}) {
+    return (dispatch) => eos.transaction({
+      actions: [
+        {
+          account: transferTokenAccount,
+          name: 'abcfgfgf',
+          authorization: [auth],
+          data: {
+            from: from,
+            to: contract,
+            quantity: quantity,
+            memo: memo,
+          }
+        },
+      ],
+    }).then(r => {
+      dispatch({ type: 'TRANSFER_SUCCEEDED', r });
+      callback(r);
+    }).catch(e => {
+      dispatch({ type: 'TRANSFER_FAILED', e });
+      callback(e);
+    });}
+
+
+
+  handleShow() {
+    this.setState({ showModel: true });
+  }
+
+  handleHide() {
+    this.setState({ showModel: false });
+  }
+  acceptContract(){
+    let result=  this.props.pushTransfer(this.props.eos,contract,transferTokenAccount, from, quantity, memo, auth)
+    if(result.type==='SUCCEEDED')
+            {
+              this.setState({ showAlertSucess: true });
+            }
+            else{
+              this.setState({ showAlertFailure: true });
+            }
+  }
+
+  
+
+  
   render() {
+
+
     return (
       <div
         style={{
@@ -58,15 +132,15 @@ class Invoice extends Component {
     <Panel.Body>
   <ListGroup>
   <ListGroupItem>Item 1  
-     <Button bsStyle="success" bsSize="small" style={inputStyle1}>Default</Button>
+     <Button bsStyle="success" bsSize="small" style={inputStyle1}  onClick={this.handleShow}>Accept</Button>
      <span style={inputStyle2}>900</span>
   </ListGroupItem>
   <ListGroupItem>Item 2  
-     <Button bsStyle="success" bsSize="small" style={inputStyle1}>Default</Button>
+     <Button bsStyle="success" bsSize="small" style={inputStyle1} onClick={this.handleShow}>Accept</Button>
      <span style={inputStyle2}>900</span>
   </ListGroupItem>
   <ListGroupItem>Item 3  
-     <Button bsStyle="success" bsSize="small" style={inputStyle1}>Default</Button>
+     <Button bsStyle="success" bsSize="small" style={inputStyle1}  onClick={this.handleShow}  >Accept</Button>
      <span style={inputStyle2}>900</span>
   </ListGroupItem>
 </ListGroup>
@@ -78,20 +152,63 @@ class Invoice extends Component {
     </Panel.Heading>
     <Panel.Body>  <ListGroup>
   <ListGroupItem>Item 1  
-     <Button bsStyle="success" bsSize="small" style={inputStyle1}>Default</Button>
+     <Button bsStyle="success" bsSize="small" style={inputStyle1}  onClick={this.handleShow}>Accept</Button>
      <span style={inputStyle2}>900</span>
   </ListGroupItem>
   <ListGroupItem>Item 2  
-     <Button bsStyle="success" bsSize="small" style={inputStyle1}>Default</Button>
+     <Button bsStyle="success" bsSize="small" style={inputStyle1}  onClick={this.handleShow}>Accept</Button>
      <span style={inputStyle2}>900</span>
   </ListGroupItem>
   <ListGroupItem>Item 3  
-     <Button bsStyle="success" bsSize="small" style={inputStyle1}>Default</Button>
+     <Button bsStyle="success" bsSize="small" style={inputStyle1}  onClick={this.handleShow}>Accept</Button>
      <span style={inputStyle2}>900</span>
   </ListGroupItem>
 </ListGroup>
 </Panel.Body>
   </Panel>
+
+          <Modal
+          {...this.props}
+          show={this.state.showModel}
+          onHide={this.handleHide}
+          dialogClassName="custom-modal"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-lg">
+              Modal heading
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h4>Proceed towards transaction</h4>
+            <p>
+                Your transaction is insured in case of default
+            </p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button bsStyle="primary" onClick={this.handleHide}>Accept</Button>
+            <Button bsStyle="danger" onClick={this.handleHide}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+        {/* if( !showAlertSucess:false)
+        {
+
+        }
+      showAlertFailure:false,) */}
+          {/* <Alert bsStyle="danger"      
+               {...this.props}
+          show={this.state.showAlertFailure}
+          >
+          <h4>Error</h4>
+          <p>
+              Error occcurred during connection
+          </p>
+
+        </Alert> */}
+
+
+
+
+
 </div>
 
 
@@ -107,4 +224,7 @@ class Invoice extends Component {
   }
 }
 
+
 export default (Invoice);
+
+
